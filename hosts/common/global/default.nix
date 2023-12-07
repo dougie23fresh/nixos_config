1d8bd58438fc694cebd7cfc7d6b19a01bc9ec33a
dougie23fresh <melvin.douglas@gmail.com>
2023-12-06 22:28:25 -0500

    fix

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -1,23 +1,9 @@
 { inputs, outputs, ... }: 
 {
   imports = [
-    ./acme.nix
-    ./auto-upgrade.nix
-    ./fish.nix
-    ./locale.nix
-    ./nix.nix
-    ./openssh.nix
-    ./optin-persistence.nix
-    ./podman.nix
-    ./sops.nix
-    ./ssh-serve-store.nix
-    ./steam-hardware.nix
-    ./systemd-initrd.nix
-    ./tailscale.nix
-    ./gamemode.nix
-    ./time.nix
     ./locale.nix
     ./location.nix
+    ./time.nix
 
     # Packages
     ./packages/default.nix
