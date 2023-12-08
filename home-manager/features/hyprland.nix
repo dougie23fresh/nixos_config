0faea7be409c006090d2c1693fb8e23a5ae6f0e4
dougie23fresh <melvin.douglas@gmail.com>
2023-12-07 22:35:01 -0500

    hypr

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -7,7 +7,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    #extraConfig = imporft ./hyprland-config.nix {};
+    extraConfig = imporft ./hyprland-config.nix {};
     xwayland.enable = true;
   };
   
