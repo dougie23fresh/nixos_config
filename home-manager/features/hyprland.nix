1e1dee461e950a20f90156a3900d8529809d08fe
dougie23fresh <melvin.douglas@gmail.com>
2023-12-07 22:08:28 -0500

    fff

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -7,14 +7,14 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    #extraConfig = import ./hyprland-config.nix {};
+    #extraConfig = imporft ./hyprland-config.nix {};
     xwayland.enable = true;
   };
   
-  xdg.portal = {
+  #xdg.portal = {
     #extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
     #configPackages = [ pkgs.inputs.hyprland.hyprland ];
-  };
+  #};
 
   home.packages = with pkgs; [
     
