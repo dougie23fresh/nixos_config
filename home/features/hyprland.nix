c813489c9ecec4d97e2bd0c506ec0bad8c5e7d1a
dougie23fresh <melvin.douglas@gmail.com>
2023-12-08 14:37:25 -0500

    fix

--- home/features/hyprland.nix
+++ home/features/hyprland.nix
@@ -112,7 +112,7 @@
 
         # Move focus with mainMod + arrow keys
         "SUPER, left, movefocus, l"
-        "SUPER, right, movefocus, r"
+        #"SUPER, right, movefocus, r"
         "SUPER, up, movefocus, u"
         "SUPER, down, movefocus, d"
 
