66f7e7907d37e8d1bfe9fd493e95b0268f46e587
dougie23fresh <melvin.douglas@gmail.com>
2023-12-07 22:07:29 -0500

    fff

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -12,7 +12,7 @@
   };
   
   xdg.portal = {
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    #extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
     #configPackages = [ pkgs.inputs.hyprland.hyprland ];
   };
 
