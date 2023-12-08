2d1c6c3f693098eae840cf10d28f260124492301
dougie23fresh <melvin.douglas@gmail.com>
2023-12-08 14:33:32 -0500

    fix

---
+++ home/features/temp.nix
@@ -0,0 +1,167 @@
+settings = {
+      primary = {
+        mode = "dock";
+        layer = "top";
+        height = 40;
+        margin = "6";
+        position = "top";
+        modules-left = [
+          "custom/menu"
+          "hyprland/workspaces"
+          "hyprland/submap"
+          "custom/currentplayer"
+          "custom/player"
+        ];
+
+        modules-center = [
+          "pulseaudio"
+          "battery"
+          "clock"
+        ];
+
+        modules-right = [
+          "network"
+          "tray"
+          "custom/hostname"
+        ];
+        clock = {
+          interval = 1;
+          format = "{:%d/%m %H:%M:%S}";
+          format-alt = "{:%Y-%m-%d %H:%M:%S %z}";
+          on-click-left = "mode";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-muted = "   0%";
+          format-icons = {
+            headphone = "󰋋";
+            headset = "󰋎";
+            portable = "";
+            default = [ "" "" "" ];
+          };
+          on-click = pavucontrol;
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰒳";
+            deactivated = "󰒲";
+          };
+        };
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+        "custom/menu" = {
+          return-type = "json";
+          exec = jsonOutput "menu" {
+            text = "";
+            tooltip = ''$(${cat} /etc/os-release | ${grep} PRETTY_NAME | ${cut} -d '"' -f2)'';
+          };
+          on-click-left = "${wofi} -S drun -x 10 -y 10 -W 25% -H 60%";
+          on-click-right = "${hyprlandd} dispatch togglespecialworkspace";
+        };
+        "custom/hostname" = {
+          exec = "echo $USER@$HOSTNAME";
+          on-click = "${systemctl} --user restart waybar";
+        };
+        "custom/currentplayer" = {
+          interval = 2;
+          return-type = "json";
+          exec = jsonOutput "currentplayer" {
+            pre = ''
+              player="$(${playerctl} status -f "{{playerName}}" 2>/dev/null || echo "No player active" | ${cut} -d '.' -f1)"
+              count="$(${playerctl} -l 2>/dev/null | ${wc} -l)"
+              if ((count > 1)); then
+                more=" +$((count - 1))"
+              else
+                more=""
+              fi
+            '';
+            alt = "$player";
+            tooltip = "$player ($count available)";
+            text = "$more";
+          };
+          format = "{icon}{}";
+          format-icons = {
+            "No player active" = " ";
+            "Celluloid" = "󰎁 ";
+            "spotify" = "󰓇 ";
+            "ncspot" = "󰓇 ";
+            "qutebrowser" = "󰖟 ";
+            "firefox" = " ";
+            "discord" = " 󰙯 ";
+            "sublimemusic" = " ";
+            "kdeconnect" = "󰄡 ";
+            "chromium" = " ";
+          };
+          on-click = "${playerctld} shift";
+          on-click-right = "${playerctld} unshift";
+        };
+        "custom/player" = {
+          exec-if = "${playerctl} status 2>/dev/null";
+          exec = ''${playerctl} metadata --format '{"text": "{{title}} - {{artist}}", "alt": "{{status}}", "tooltip": "{{title}} - {{artist}} ({{album}})"}' 2>/dev/null '';
+          return-type = "json";
+          interval = 2;
+          max-length = 30;
+          format = "{icon} {}";
+          format-icons = {
+            "Playing" = "󰐊";
+            "Paused" = "󰏤 ";
+            "Stopped" = "󰓛";
+          };
+          on-click = "${playerctl} play-pause";
+        };
+      };
+    };
+    style = ''
+      * {
+        min-height: 0;
+      }
+
+      window#waybar {
+        font-family: 'Inter', 'RobotoMono Nerd Font';
+        font-size: 12px;
+      }
+
+      tooltip {
+      }
+
+      #custom-nix {
+        padding: 2px 6px;
+      }
+
+      #workspaces button {
+        padding: 2px 6px;
+        margin: 0 6px 0 0;
+      }
+
+      .modules-right * {
+        padding: 0 6px;
+        margin: 0 0 0 4px;
+      }
+
+      #mpris {
+        padding: 0 6px;
+      }
+
+      #custom-notification {
+        padding: 0 6px 0 6px;
+      }
+
+      #tray {
+        padding: 0 6px;
+      }
+      
+      #tray * {
+        padding: 0;
+        margin: 0;
+      }
+    '';
