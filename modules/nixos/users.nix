8c39d99cbe52892ebbf36702283d511a91dc1285
dougie23fresh <melvin.douglas@gmail.com>
2023-12-17 19:42:42 -0500

    hfghfg

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -10,16 +10,16 @@ in
       type = types.str;
       default = "nixuser";
     };
-    userDescription = mkOption {
-      type = types.str;
-      default = "nixuser";
-    };
+    #userDescription = mkOption {
+    #  type = types.str;
+    #  default = "nixuser";
+    #};
   };
 
   config = mkIf cfg.enable {
     users.users.${cfg.userName} = {
       isNormalUser = true;
-      description = "testttttt";
+      description = "yyyyyyyyy"; # "${cfg.userDescription}";
       extraGroups = [ "networkmanager" "wheel" ];
       #shell = pkg.zsh;
       packages = with pkgs; [
