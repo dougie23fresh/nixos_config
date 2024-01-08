773f1e2edc83dd173fd6e553cac93105cdfd8660
dougie23fresh <melvin.douglas@gmail.com>
2024-01-08 18:56:05 -0500

    Update obs.nix

--- modules/home-manager/video/obs.nix
+++ modules/home-manager/video/obs.nix
@@ -32,13 +32,38 @@ in
   };
 
   config = mkIf cfg.enable {
+    # boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+    #boot.extraModprobeConfig = ''
+    #  options v4l2loopback devices=1 video_nr=13 card_label="OBS Virtual Camera" exclusive_caps=1
+    #'';
     environment.systemPackages = [
-      (pkgs.wrapOBS {
-        plugins = with pkgs.obs-studio-plugins; [
+      (unstable.wrapOBS {
+        plugins = with unstable.obs-studio-plugins; [
           wlrobs
           obs-multi-rtmp
-          obs-move-transition
           looking-glass-obs
+          advanced-scene-switcher
+          obs-3d-effect
+          one-command-source
+          obs-gradient-source
+          obs-gstreamer
+          obs-move-transition
+          obs-mute-filter
+          obs-pipewire-audio-capture
+          obs-rgb-levels-filter
+          obs-scale-to-sound
+          obs-shaderfilter
+          obs-source-clone
+          obs-source-record
+          obs-source-switcher
+          obs-teleport
+          obs-text-pthread
+          obs-transition-table
+          obs-vaapi
+          obs-vertical-canvas
+          obs-vintage-filter
+          obs-websocket
+          waveform
         ];
       })
     ];
