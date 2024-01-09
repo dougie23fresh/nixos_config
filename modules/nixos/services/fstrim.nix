a09e7511d38e090343fde3cbc59de813ec9936af
dougie23fresh <melvin.douglas@gmail.com>
2024-01-08 19:05:49 -0500

    Update fstrim.nix

--- modules/nixos/services/fstrim.nix
+++ modules/nixos/services/fstrim.nix
@@ -10,5 +10,12 @@ in
 
   config = mkIf cfg.enable {
     services.fstrim.enable = true;
+environment.systemPackages = with pkgs; [
+    nvme-cli
+    smartmontools
+  ] ++ lib.optionals (desktop != null) [
+    gsmartcontrol
+  ];
+  services.smartd.enable = true;
   };
 }
