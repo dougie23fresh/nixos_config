5ee924345600bacf3b153461c740e59dba225cab
dougie23fresh <melvin.douglas@gmail.com>
2024-01-08 19:17:43 -0500

    Update other.nix

--- modules/home-manager/terminal/other.nix
+++ modules/home-manager/terminal/other.nix
@@ -69,3 +69,72 @@ Terminal webcam
       ramfetch # Terminal system info
       vdpauinfo # Terminal VDPAU info
       wavemon # Terminal WiFi monitor
+
+micro";
+    };
+  };
+
+  programs = {
+    aria2 = {
+      enable = true;
+    };
+    atuin = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      flags = [
+        "--disable-up-arrow"
+      ];
+      package = pkgs.atuin;
+      settings = {
+        auto_sync = true;
+        dialect = "uk";
+        show_preview = true;
+        style = "compact";
+        sync_frequency = "1h";
+        sync_address = "https://api.atuin.sh";
+        update_check = false;
+      };
+    };
+bottom = {
+      enable = true;
+      settings = {
+        colors = {
+          high_battery_color = "green";
+          medium_battery_color = "yellow";
+          low_battery_color = "red";
+        };
+        disk_filter = {
+          is_list_ignored = true;
+          list = [ "/dev/loop" ];
+          regex = true;
+          case_sensitive = false;
+          whole_word = false;
+        };
+        flags = {
+          dot_marker = false;
+          enable_gpu_memory = true;
+          group_processes = true;
+          hide_table_gap = true;
+          mem_as_value = true;
+          tree = true;
+        };
+      };
+    };
+  yt-dlp = {
+      enable = true;
+      package = pkgs.unstable.yt-dlp;
+      settings ={
+        audio-format = "best";
+        audio-quality = 0;
+        embed-chapters = true;
+        embed-metadata = true;
+        embed-subs = true;
+        embed-thumbnail = true;
+        remux-video = "aac>m4a/mov>mp4/mkv";
+        sponsorblock-mark = "sponsor";
+        sub-langs = "all";
+      };
+    };
+
