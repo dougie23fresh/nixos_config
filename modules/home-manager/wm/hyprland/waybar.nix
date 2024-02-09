909a0680c6a5df2980da23fcb1d4bfe810a1ddd6
dougie23fresh <melvin.douglas@gmail.com>
2024-02-08 21:37:26 -0500

    dfsf

--- modules/home-manager/wm/hyprland/waybar.nix
+++ modules/home-manager/wm/hyprland/waybar.nix
@@ -172,7 +172,7 @@ in
             };
             #scroll-step = 10;
             #on-click = "myxer";
-            on-click = "pavucontrol";
+            on-click = "${pkgs.pavucontrol}/bin/pavucontrol &";
             # tooltip-format = "At {volume}%";
           };
           backlight = {
@@ -219,7 +219,7 @@ in
           "custom/dunst" = {
             format = "{}";
             exec = "~/.config/hypr/themes/minimal/waybar/modules/dunst.sh";
-            on-click = "dunstctl set-paused toggle";
+            on-click = "${pkgs.dunstctl}/bin/dunstctl set-paused toggle";
             restart-interval = 1;
           };
           "custom/lock" = {
