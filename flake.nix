788bc33b8c72c14e7620f15c4ccf9c3743b6127c
dougie23fresh <melvin.douglas@gmail.com>
2024-02-15 19:11:02 -0500

    dfdf

--- flake.nix
+++ flake.nix
@@ -24,19 +24,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Hyprland
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+    #hyprland.url = "github:hyprwm/Hyprland";
+    #hyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     # Community scripts and utilities for Hypr projects
-    hyprwm-contrib.url = "github:hyprwm/contrib";
-    hyprwm-contrib.inputs.nixpkgs.follows = "nixpkgs";
+    #hyprwm-contrib.url = "github:hyprwm/contrib";
+    #hyprwm-contrib.inputs.nixpkgs.follows = "nixpkgs";
 
-    hyprland-plugins.url = "github:misterio77/hyprland-plugins/flake-winwrap";
-    hyprland-plugins.inputs.hyprland.follows = "hyprland";
+    #hyprland-plugins.url = "github:misterio77/hyprland-plugins/flake-winwrap";
+    #hyprland-plugins.inputs.hyprland.follows = "hyprland";
 
     # Nix Gaming
-    nix-gaming.url = "github:fufexan/nix-gaming";
-    nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
+    #nix-gaming.url = "github:fufexan/nix-gaming";
+    #nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
