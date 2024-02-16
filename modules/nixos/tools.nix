e2ef284ee7f1014bcbc6fca0ee25f14ba4783b5f
dougie23fresh <melvin.douglas@gmail.com>
2024-02-15 22:10:08 -0500

    kkk

--- modules/nixos/tools.nix
+++ modules/nixos/tools.nix
@@ -13,20 +13,20 @@ in
   };
 
   config = mkIf cfg.enable {
-    
+    pkgs.config.packageOverrides = super: {
+      python3 = super.python3.override {
+        packageOverrides = python-self: python-super: {
+          ipython = python-super.ipython.overridePythonAttrs (oldAttrs: {
+            disabled = pythonOlder "3.9";
+          });
+        };
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       #(python311.withPackages my-python-packages)
       #python311Packages.ipython
-      nixpkgs.config.packageOverrides = super: {
-        python3 = super.python3.override {
-          packageOverrides = python-self: python-super: {
-            ipython = python-super.ipython.overridePythonAttrs (oldAttrs: {
-              disabled = pythonOlder "3.9";
-            });
-          };
-        };
-      };
+      
       fzf
       killall
       unzip
