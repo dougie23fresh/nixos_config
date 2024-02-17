36dd556619b57ea26f815cd6462a310a1efde3fc
dougie23fresh <melvin.douglas@gmail.com>
2024-02-17 14:43:11 -0500

    inherit username;

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.extraSpecialArgs = { inherit inputs; inherit username;};
             home-manager.users.${username} = import ./modules/home/default.nix;
           }
         ];
