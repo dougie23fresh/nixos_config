90c72a98c14feb923c2feecbf3e2e77bc124898d
dougie23fresh <melvin.douglas@gmail.com>
2024-02-17 16:44:24 -0500

    Update wm_hyperland_waybar.nix

--- modules/home/wm_hyperland_waybar.nix
+++ modules/home/wm_hyperland_waybar.nix
@@ -1,4 +1,41 @@
 { inputs, outputs, pkgs, ... }:
+with lib;
+let 
+  cfg = config.dougieHome.wm.waybar;
+
+  cat = "${pkgs.coreutils}/bin/cat";
+  cut = "${pkgs.coreutils}/bin/cut";
+  find = "${pkgs.findutils}/bin/find";
+  grep = "${pkgs.gnugrep}/bin/grep";
+  pgrep = "${pkgs.procps}/bin/pgrep";
+  tail = "${pkgs.coreutils}/bin/tail";
+  wc = "${pkgs.coreutils}/bin/wc";
+  xargs = "${pkgs.findutils}/bin/xargs";
+  timeout = "${pkgs.coreutils}/bin/timeout";
+  ping = "${pkgs.iputils}/bin/ping";
+
+  jq = "${pkgs.jq}/bin/jq";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+  journalctl = "${pkgs.systemd}/bin/journalctl";
+  playerctl = "${pkgs.playerctl}/bin/playerctl";
+  playerctld = "${pkgs.playerctl}/bin/playerctld";
+  pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
+  wofi = "${pkgs.wofi}/bin/wofi";
+  hyprlandd = "${pkgs.hyprland}/bin/hyprctl";
+
+  # Function to simplify making waybar outputs
+  jsonOutput = name: { pre ? "", text ? "", tooltip ? "", alt ? "", class ? "", percentage ? "" }: "${pkgs.writeShellScriptBin "waybar-${name}" ''
+    set -euo pipefail
+    ${pre}
+    ${jq} -cn \
+      --arg text "${text}" \
+      --arg tooltip "${tooltip}" \
+      --arg alt "${alt}" \
+      --arg class "${class}" \
+      --arg percentage "${percentage}" \
+      '{text:$text,tooltip:$tooltip,alt:$alt,class:$class,percentage:$percentage}'
+  ''}/bin/waybar-${name}";
+in
 {
   systemd.user.services.waybar = {
       Unit.StartLimitBurst = 30;
