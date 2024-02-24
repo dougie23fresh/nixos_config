9f4cf5a5dcf0a2eea60ca98ba832f80850861f7a
dougie23fresh <melvin.douglas@gmail.com>
2024-02-23 23:56:46 -0500

    keyboard sleep

--- config/ceres/system.nix
+++ config/ceres/system.nix
@@ -9,6 +9,13 @@
       ../../modules/system/nvidia-gpu.nix
   
     ];
-
+  services.udev.extraRules = ''
+      # keyboard disable autosuspand
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="062a", ATTR{idProduct}=="4101", ATTR{power/autosuspend}="-1"
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="3434", ATTR{idProduct}=="0430", ATTR{power/autosuspend}="-1"
+    # mouse disable autosuspand
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c548", ATTR{power/autosuspend}="-1"
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c52b ", ATTR{power/autosuspend}="-1"
+  '';
   system.stateVersion = "23.05";
 }
