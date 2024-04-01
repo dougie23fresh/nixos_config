7dc0ca7f4052aac018e7a37140889e10b6a27c9c
dougie23fresh <melvin.douglas@gmail.com>
2024-04-01 18:46:04 -0400

    ll

--- modules/system/window-manager/plasma6.nix
+++ modules/system/window-manager/plasma6.nix
@@ -1,4 +1,9 @@
 {  lib, config, pkgs, inputs, ... }:
 {
   services.desktopManager.plasma6.enable = true;
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [
+    kdePackages.qt6ct
+    kdePackages.qtstyleplugin-kvantum
+  ];
 }
