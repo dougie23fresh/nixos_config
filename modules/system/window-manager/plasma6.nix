91d65bd104dcdf451d0f5d28da0c59639160ed21
dougie23fresh <melvin.douglas@gmail.com>
2024-04-01 19:13:56 -0400

    kk

--- modules/system/window-manager/plasma6.nix
+++ modules/system/window-manager/plasma6.nix
@@ -2,6 +2,8 @@
 {
   services.desktopManager.plasma6.enable = true;
   programs.dconf.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+
   environment.systemPackages = with pkgs; [
     kdePackages.qt6ct
     kdePackages.qtstyleplugin-kvantum
