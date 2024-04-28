9ddbb9d08fa3345bc917b5cde1085cd85df8d3ff
Melvin <melvin.douglas@gmail.com>
2024-04-28 15:09:30 -0400

    fdf

--- config/hpelitebook/configuration.nix
+++ config/hpelitebook/configuration.nix
@@ -9,47 +9,43 @@
   catppuccin.flavour = "macchiato";
   imports = [
     ./hardware.nix
-    outputs.nixosModules.systemhardware.cpu-intel
-    outputs.nixosModules.systemhardware.gpu-intel
-    outputs.nixosModules.system.hardware.bluetooth
-    outputs.nixosModules.system.hardware.firmware
-    outputs.nixosModules.system.hardware.keyboard-backlight
-    outputs.nixosModules.system.hardware.logitech
-    #outputs.nixosModules..system.hardware.sscanner
-    outputs.nixosModules.system.hardware.sound
-    outputs.nixosModules.system.hardware.power-management
+    outputs.nixosModules.hardware.bluetooth
+    outputs.nixosModules.hardware.cpu-intel
+    outputs.nixosModules.hardware.gpu-intel
+    outputs.nixosModules.hardware.firmware
+    outputs.nixosModules.hardware.keyboard-backlight
+    outputs.nixosModules.hardware.logitech
+    #outputs.nixosModules.hardware.scanner
+    outputs.nixosModules.hardware.power-management
+    outputs.nixosModules.hardware.sound
 
-    outputs.nixosModules.system.systems.nix # refactor
-    outputs.nixosModules.system.users
-    #outputs.nixosModules..system.laptop.nix # refactor
+    outputs.nixosModules.system # refactor
+    outputs.nixosModules.users
+    outputs.nixosModules.fonts # refactor
 
-
-    outputs.nixosModules.system.default_pkgs.nix # refactor
-    outputs.nixosModules.system.default_programs.nix # refactor
-    outputs.nixosModules.system.font.nix # refactor
-    #..hardware
     ########### services
     #outputs.nixosModules..system.services.nix # refactor
-    outputs.nixosModules.system.services.dbus
-    outputs.nixosModules.system.services.device-manager
-    outputs.nixosModules.system.services.fingerprint
-    outputs.nixosModules.system.services.firmware-update
-    outputs.nixosModules.system.services.flatpak
-    outputs.nixosModules.system.services.hard-drive
-    outputs.nixosModules.system.services.mdns
-    outputs.nixosModules.system.services.openrgb
-    outputs.nixosModules.system.services.openssh
-    outputs.nixosModules.system.services.out-of-memory
-    outputs.nixosModules.system.services.printing
+    outputs.nixosModules.services.clipboard
+    outputs.nixosModules.services.dbus
+    outputs.nixosModules.services.device-manager
+    outputs.nixosModules.services.fingerprint
+    outputs.nixosModules.services.firmware-update
+    outputs.nixosModules.services.flatpak
+    outputs.nixosModules.services.hard-drive
+    outputs.nixosModules.services.mdns
+    outputs.nixosModules.services.openrgb
+    outputs.nixosModules.services.openssh
+    outputs.nixosModules.services.out-of-memory
+    outputs.nixosModules.services.printing
     #outputs.nixosModules..system.services.redshift
-    outputs.nixosModules.system.services.security
-    outputs.nixosModules.system.services.smartd
-    outputs.nixosModules.system.services.ssd
-    outputs.nixosModules.system.services.syncthing
-    outputs.nixosModules.system.services.tailscale
-    outputs.nixosModules.system.services.tumbler
-    outputs.nixosModules.system.services.udisk
-    #outputs.nixosModules..system.services.yubikey
+    outputs.nixosModules.services.security
+    outputs.nixosModules.services.smartd
+    outputs.nixosModules.services.ssd
+    outputs.nixosModules.services.syncthing
+    outputs.nixosModules.services.tailscale
+    outputs.nixosModules.services.tumbler
+    outputs.nixosModules.services.udisk
+    #outputs.nixosModules.services.yubikey
     # virtualisation
     #outputs.nixosModules..system.virtualisation.distrobox
     #outputs.nixosModules..system.virtualisation.docker
@@ -58,21 +54,21 @@
     #outputs.nixosModules..system.virtualisation.waydroid
     # window-manager\
     #outputs.nixosModules..system.display-manager.gdm
-    outputs.nixosModules.system.display-manager.sddm
+    outputs.nixosModules.display-manager.sddm
 
-    outputs.nixosModules.system.window-manager.default
-    outputs.nixosModules.system.window-manager.hyprland
+    outputs.nixosModules.window-manager.xserver
+    outputs.nixosModules.window-manager.hyprland
     #outputs.nixosModules..system.window-manager.plasma5
     #outputs.nixosModules..system.window-manager.plasma6
-    outputs.nixosModules.system.window-manager.xfce
-    outputs.nixosModules.system.window-manager.plasma6
+    outputs.nixosModules.window-manager.xfce
+    outputs.nixosModules.window-manager.plasma6
     #LXQt
     #Enlightenment
     #budgie
-    outputs.nixosModules.system.base-apps.archives
-    outputs.nixosModules.system.base-apps.base
-    outputs.nixosModules.system.base-apps.network
-    outputs.nixosModules.system.base-apps.nixapp
+    outputs.nixosModules.base-apps.archives
+    outputs.nixosModules.base-apps.base
+    outputs.nixosModules.base-apps.network
+    outputs.nixosModules.base-apps.nixapp
 
   ];
 
--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -1,4 +1,16 @@
 {
-  gdm = import ./gdm.nix;
-  sddm = import ./sddm.nix;
+  bluetooth = import ./bluetooth.nix;
+  cpu-amd = import ./cpu-amd.nix;
+  cpu-intel = import ./cpu-intel.nix;
+  firmware = import ./firmware.nix;
+  gpu-amd = import ./gpu-amd.nix;
+  gpu-intel-nvidia = import ./gpu-intel-nvidia.nix;
+  gpu-intel = import ./gpu-intel.nix;
+  gpu-nvidia = import ./gpu-nvidia.nix;
+  i2c = import ./i2c.nix;
+  keyboard-backlight = import ./keyboard-backlight.nix;
+  logitech = import ./logitech.nix;
+  power-management = import ./power-management.nix;
+  scanner = import ./scanner.nix;
+  sound = import ./sound.nix;
 }
