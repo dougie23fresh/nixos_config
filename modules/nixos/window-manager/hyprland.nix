bf28f3ab22d1bec2761f4e717e3a258756d592a9
dougie23fresh <melvin.douglas@gmail.com>
2024-05-01 20:35:56 -0400

    GH

--- modules/nixos/window-manager/hyprland.nix
+++ modules/nixos/window-manager/hyprland.nix
@@ -24,13 +24,13 @@
     polkit_gnome
     gnome.nautilus
     gnome.zenity
-    (pkgs.writeScriptBin "hypr-run" ''
-    export XDG_SESSION_TYPE="wayland"
-    export XDG_SESSION_DESKTOP="Hyprland"
-    export XDG_CURRENT_DESKTOP="Hyprland"
-    systemd-run --user --scope --collect --quiet --unit="hyprland" \
-        systemd-cat --identifier="hyprland" ${pkgs.hyprland}/bin/Hyprland $@
-    ${pkgs.hyprland}/bin/hyperctl dispatch exit
-    '')
+    #(pkgs.writeScriptBin "hypr-run" ''
+    ##export XDG_SESSION_TYPE="wayland"
+    #export XDG_SESSION_DESKTOP="Hyprland"
+    #export XDG_CURRENT_DESKTOP="Hyprland"
+    #systemd-run --user --scope --collect --quiet --unit="hyprland" \
+    #    systemd-cat --identifier="hyprland" ${pkgs.hyprland}/bin/Hyprland $@
+    #${pkgs.hyprland}/bin/hyperctl dispatch exit
+    #'')
   ];
 }
