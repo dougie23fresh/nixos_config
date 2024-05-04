daa7073c99dbfa990cfb981400196c4e6a5adead
Melvin <melvin.douglas@gmail.com>
2024-05-04 13:27:45 -0400

    ff

--- modules/home-manager/window-manager/xdg.nix
+++ modules/home-manager/window-manager/xdg.nix
@@ -13,11 +13,11 @@
         common.default = ["gtk"];
         hyprland.default = ["gtk" "hyprland"];
       };
-      #extraPortals = with pkgs; [
+      extraPortals = with pkgs; [
       #  #xdg-desktop-portal-hyprland
       #  xdg-desktop-portal-gtk
-      #  #libsForQt5.xdg-desktop-portal-kde
-      #];
+        kdePackages.xdg-desktop-portal-kde
+      ];
     };
   };
 }
