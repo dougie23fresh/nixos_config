e1dc9e99c22b3e44780111a5942a51268d00ef79
dougie23fresh <melvin.douglas@gmail.com>
2024-05-04 20:04:53 -0400

    kj

--- modules/home-manager/networking/browsers/vivaldi.nix
+++ modules/home-manager/networking/browsers/vivaldi.nix
@@ -4,8 +4,10 @@
     (vivaldi.override{
       proprietaryCodecs = true;
       enableWidevine = true;
+       commandLineArgs = "--disable-features=AllowQt";
     })
     vivaldi-ffmpeg-codecs
     widevine-cdm
+    #--disable-features=AllowQt
   ];
 }
