996945f8069cb673bf53ac2221d3ff4dc1573bc1
Melvin <melvin.douglas@gmail.com>
2024-05-06 22:13:01 -0400

    f

--- modules/home-manager/networking/browsers/brave.nix
+++ modules/home-manager/networking/browsers/brave.nix
@@ -4,4 +4,15 @@
     brave
 
   ];
+  programs.brave.enable = true;
+  programs.brave.extensions = [
+    #"cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+    #"nngceckbapebfimnlniiiahkandclblb" # bitwarden
+    #"pcmpcfapbekmbjjkdalcgopdkipoggdi" # markdownload
+    #"mlmoighangjfhiookjppplipefbbnaoo" # half-bold
+    {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # Ublock Origin
+    {id = "nngceckbapebfimnlniiiahkandclblb";} # Bitwarden
+    {id = "pcmpcfapbekmbjjkdalcgopdkipoggdi";} # markdownload
+    {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";} # Vimium C
+  ];
 }
--- modules/home-manager/networking/browsers/chromium.nix
+++ modules/home-manager/networking/browsers/chromium.nix
@@ -2,10 +2,14 @@
 {
   programs.chromium.enable = true;
   programs.chromium.extensions = [
-    "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
-    "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-    "pcmpcfapbekmbjjkdalcgopdkipoggdi" # markdownload
+    #"cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+    #"nngceckbapebfimnlniiiahkandclblb" # bitwarden
+    #"pcmpcfapbekmbjjkdalcgopdkipoggdi" # markdownload
     #"mlmoighangjfhiookjppplipefbbnaoo" # half-bold
+    {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # Ublock Origin
+    {id = "nngceckbapebfimnlniiiahkandclblb";} # Bitwarden
+    {id = "pcmpcfapbekmbjjkdalcgopdkipoggdi";} # markdownload
+    {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";} # Vimium C
   ];
   home.packages = with pkgs; [
     chromium
