d0f8d19fa4435fb648afdca579e4a6f3d2977609
Melvin <melvin.douglas@gmail.com>
2024-05-09 17:19:55 -0400

    dsf

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -139,11 +139,11 @@
   #  config.nix.registry;
 
   nixpkgs.config.allowUnfree = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
+  #nix.gc = {
+  #  automatic = true;
+  #  dates = "weekly";
+  #  options = "--delete-older-than 7d";
+  #};
   nix.settings = {
     warn-dirty = false;
     auto-optimise-store = true;
