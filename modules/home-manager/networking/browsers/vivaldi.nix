3c6bb1416ca5e94d485f39cebac28cc577a4725e
dougie23fresh <melvin.douglas@gmail.com>
2024-05-19 00:09:26 -0400

    viv

--- modules/home-manager/networking/browsers/vivaldi.nix
+++ modules/home-manager/networking/browsers/vivaldi.nix
@@ -4,7 +4,7 @@
     (vivaldi.override{
       proprietaryCodecs = true;
       enableWidevine = true;
-      #commandLineArgs = "--disable-features=AllowQt";
+      commandLineArgs = "--enable-features=UseOzonePlatform --enable-features=WaylandWindowDecorations --ozone-platform=wayland  --disable-features=AllowQt";
     })
     vivaldi-ffmpeg-codecs
     widevine-cdm
