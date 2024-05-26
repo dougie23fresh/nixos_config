d9fb399550efcdf6fdb6df39125db0e547f38779
dougie23fresh <melvin.douglas@gmail.com>
2024-05-26 00:40:48 -0400

    GGG

--- modules/home-manager/window-manager/labwc.nix
+++ modules/home-manager/window-manager/labwc.nix
@@ -16,7 +16,7 @@
       #  source = ./labwc/rc.xml;
       #  onChange = "${pkgs.psmisc}/bin/killall -s SIGHUP labwc || true";
       #};
-      "labwc/waybar_config.json".source = ",/labwc/config.json";
+      "labwc/waybar_config.json".source = "./labwc/config.json";
       "labwc/waybar_style.css".source = "~/nixos_config/modules/home-manager/window-manager/labwc/style.json";
       "labwc/autostart" = {
         text = ''
