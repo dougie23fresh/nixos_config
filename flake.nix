850c2b4b985d9743dec2f19df24fb69447585669
Melvin <melvin.douglas@gmail.com>
2024-06-29 10:19:40 -0400

    dsf

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Melvin's Nix Config";
   inputs = {
     #nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hardware.url = "github:nixos/nixos-hardware";
