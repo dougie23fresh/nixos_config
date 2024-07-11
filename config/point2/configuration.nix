61fda29f29dd7b22c4ea6c881e2cbe50e9ad2415
dougie23fresh <melvin.douglas@gmail.com>
2024-07-11 18:19:02 -0400

    sf

--- config/point2/configuration.nix
+++ config/point2/configuration.nix
@@ -52,8 +52,8 @@
     outputs.nixosModules.base-apps.nixapp
 
   ];
-  services.unifi.enable = true;
-  services.unifi.unifiPackage = pkgs.unifi8;
+  #services.unifi.enable = true;
+  #services.unifi.unifiPackage = pkgs.unifi8;
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/melvin/.config/sops/age/keys.txt";
