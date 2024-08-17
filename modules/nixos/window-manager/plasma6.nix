ca9dc1403697adec9b6f7deb2bfbdc8f8c9aed8e
dougie23fresh <melvin.douglas@gmail.com>
2024-08-17 10:36:15 -0400

    sdf

--- modules/nixos/window-manager/plasma6.nix
+++ modules/nixos/window-manager/plasma6.nix
@@ -1,14 +1,20 @@
-{  pkgs, ... }:
+{  pkgs, nixpkgs-unstable,... }:
+let
+unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+
+in
 {
   services.desktopManager.plasma6.enable = true;
   programs.dconf.enable = true;
   #services.displayManager.defaultSession = "plasma";
   #services.xserver.displayManager.defaultSession = "plasma";
   #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
-  environment.systemPackages = with pkgs; [
-    kdePackages.qt6ct
-    kdePackages.qtstyleplugin-kvantum
-    kdePackages.kdeplasma-addons
+  environment.systemPackages = [
+    unstable.kdePackages.krdp  
+    
+    pkgs.kdePackages.qt6ct
+    pkgs.kdePackages.qtstyleplugin-kvantum
+    pkgs.kdePackages.kdeplasma-addons
   ];
   #environment.variables = {
   #  IXOS_OZONE_WL="1";
