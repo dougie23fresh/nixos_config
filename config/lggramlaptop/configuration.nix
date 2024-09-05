7894b4d6799320882507d05ac60674e56b154c0d
dougie23fresh <melvin.douglas@gmail.com>
2024-09-05 16:50:21 -0400

    sdf

--- config/lggramlaptop/configuration.nix
+++ config/lggramlaptop/configuration.nix
@@ -92,4 +92,12 @@
   #catppuccin.flavor = "macchiato";
   
   system.stateVersion = "23.11";
+  hardware.sane.enable = true;
+  users.users.melvin.extraGroups = [ "scanner" "lp" ];
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  services.udev.packages = [ pkgs.sane-airscan ];
+  services.ipp-usb.enable=true;
+  environment.systemPackages = with pkgs; [
+    libsForQt5.skanlite
+  ];
 }
