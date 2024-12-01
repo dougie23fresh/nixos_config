81550785d2af6434fc9e9018807326bf2e8c4477
dougie23fresh <melvin.douglas@gmail.com>
2024-12-01 00:31:27 -0500

    fff

--- config/home/home-base.nix
+++ config/home/home-base.nix
@@ -10,7 +10,7 @@
         #outputs.homeManagerModules.window-manager.waybar
         #outputs.homeManagerModules.window-manager.fuzzel
         outputs.homeManagerModules.window-manager.labwc
-        outputs.homeManagerModules.audio.spotify
+        #outputs.homeManagerModules.audio.spotify
         outputs.homeManagerModules.dev.git
         #outputs.homeManagerModules.dev.python
         outputs.homeManagerModules.dev.thonny
