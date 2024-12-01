70b2b51ec2872ce8fab0d6882a7e416e7ee5a5e9
dougie23fresh <melvin.douglas@gmail.com>
2024-11-30 22:27:32 -0500

    khk

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Melvin's Nix Config";
   inputs = {
     #nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
