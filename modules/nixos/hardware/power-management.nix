35363151b8b0e3e47970adeaa1ffc50ef7c584f6
dougie23fresh <melvin.douglas@gmail.com>
2025-01-22 18:53:44 -0500

    ggg

--- modules/nixos/hardware/power-management.nix
+++ modules/nixos/hardware/power-management.nix
@@ -19,7 +19,7 @@
   };
   services.power-profiles-daemon.enable = false; # Disable GNOMEs power management
   #powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  powerManagement.powertop.enable = true; # Enable powertop
+  #powerManagement.powertop.enable = true; # Enable powertop
   services.thermald.enable = true; # Enable thermald (only necessary if on Intel CPUs)
   # Performance and power saving
   #  services.auto-cpufreq.enable = true;
