a236fc0d0321e524c925b209fbc394d65432cf07
dougie23fresh <melvin.douglas@gmail.com>
2025-04-26 15:16:09 -0400

    rograms.starship.c atppuccin.enable

--- modules/home-manager/terminal-emulators/starship.nix
+++ modules/home-manager/terminal-emulators/starship.nix
@@ -1,6 +1,6 @@
 {
   programs.starship.enable = true;
-  programs.starship.catppuccin.enable  = true;
+  #programs.starship.catppuccin.enable  = true;
   catppuccin.starship.enable = true;
 
   programs.starship.enableZshIntegration = true;
